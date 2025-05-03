dd669bb10c05bbe6acc0849d43882ac7f8f276d9
Steven-Ou <osteve425@gmail.com>
2025-05-03 12:04:50 -0400

    creating the app and PORT

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,6 @@ const cors = require('cors');//Connects the frontend and backend
 const { exec } = require('child_process'); //Execute system commands
 const fs = require('fs');//deleting files after it is used
 const path = require('path');//to let it work with all OS
+
+const app= express(); //Creating an instance of express
+const PORT = 5000; //Port number for the server
