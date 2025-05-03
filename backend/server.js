51e0f52c13dc3e62f83203225b1a50e676470766
Steven-Ou <osteve425@gmail.com>
2025-05-03 12:07:09 -0400

    commenting

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');//Connects the frontend and backend
 const { exec } = require('child_process'); //Execute system commands
 const fs = require('fs');//deleting files after it is used
 const path = require('path');//to let it work with all OS
-
+//Setting up Express App
 const app= express(); //Creating an instance of express
 const PORT = 5000; //Port number for the server
+//Middleware
