23e568b28ed5410939f48178a2c5977a6ffd7fad
Steven-Ou <osteve425@gmail.com>
2025-05-03 12:03:36 -0400

    commenting

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@
 //Dependencies
 const express = require('express'); //Web Framework to handle HTTP requests
 const cors = require('cors');//Connects the frontend and backend
-const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+const { exec } = require('child_process'); //Execute system commands
+const fs = require('fs');//deleting files after it is used
+const path = require('path');//
