84b386a49b49dfcd399820dde0218b4968945a25
Steven-Ou <osteve425@gmail.com>
2025-05-03 12:40:20 -0400

    commenting

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,9 @@
 import {useState} from 'react';
 
 export default function Home() {
-    const [url, setUrl] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
+    const [url, setUrl] = useState(''); //storing the URL
+    const [loading, setLoading] = useState(false); //checks if the app is in the process of fetching
+    const [error, setError] = useState(''); //Stores error messsage and update the error state. 
+
+    const
 }
