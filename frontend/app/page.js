ae3afc8ebc554cd0fd01610b3d0821b0bc409771
Steven-Ou <osteve425@gmail.com>
2025-05-03 12:29:51 -0400

    removing and redoing it

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,13 +2,5 @@ import {useState} from 'react';
 
 export default function Home() {
     const [url, setUrl] = useState('');
-    const download = () => {
-        if(!url){
-            alert('Please enter a URL');
-            return;
-        }
-    }
-    const link = document.createElement('a');
-    link.href =`http://localhost:5000/download?url=${encodeURIComponent(url)}}`;
-
+    
 }
