6a4d17319034a4fae5183137155601aeac6671b5
Steven-Ou <osteve425@gmail.com>
2025-05-15 22:04:54 -0400

    Updating

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.mjs
+export default { // Note: 'export default' instead of 'module.exports'
+  plugins: {
+    tailwindcss: {}, // This plugin tells PostCSS to process Tailwind CSS
+    autoprefixer: {}, // This plugin adds vendor prefixes for broader browser support
+  },
 };
-
-export default config;
