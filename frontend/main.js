bacf9464855b1e518cca96b43bd4f6440ee4fee1
Steven-Ou <osteve425@gmail.com>
2025-05-24 14:37:53 -0400

    importing electron is dev

--- frontend/main.js
+++ frontend/main.js
@@ -2,3 +2,4 @@
 const { app, BrowserWindow} = require('electron'); // Importing app and BrowserWindow from electron
 const path = require('path'); // Importing path module
 const{spawn} = require('child_process'); // Importing spawn from child_process module
+const isDev = require('electron-is-dev'); // Importing electron-is-dev module
