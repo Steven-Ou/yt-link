ea8816178fe1dffa44be7b8c45a02f2c9be11919
Steven-Ou <osteve425@gmail.com>
2025-05-24 00:28:38 -0400

    spawning importing spawn from child_process module

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,4 @@
 //main.js
 const { app, BrowserWindow} = require('electron'); // Importing app and BrowserWindow from electron
 const path = require('path'); // Importing path module
+const{spawn} = require('child_process'); // Importing spawn from child_process module
