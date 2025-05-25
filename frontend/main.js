a1d33097b0b994a62484d80d6dfed548528b0f65
Steven-Ou <osteve425@gmail.com>
2025-05-24 22:19:26 -0400

    importing autoUpdater

--- frontend/main.js
+++ frontend/main.js
@@ -4,3 +4,4 @@ const path = require('path'); // Importing path module
 const{spawn} = require('child_process'); // Importing spawn from child_process module
 const isDev = require('electron-is-dev'); // Importing electron-is-dev module
 const tcpPortUsed = require('tcp-port-used'); // Importing tcp-port-used module
+const{autoUpdater} = require('electron-updater'); // Importing autoUpdater from electron-updater module
