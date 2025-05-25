a81e48cfe0d55bffbf34df4cc159361ca6185e02
Steven-Ou <osteve425@gmail.com>
2025-05-24 22:21:37 -0400

    adding const log

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,7 @@ const{spawn} = require('child_process'); // Importing spawn from child_process m
 const isDev = require('electron-is-dev'); // Importing electron-is-dev module
 const tcpPortUsed = require('tcp-port-used'); // Importing tcp-port-used module
 const{autoUpdater} = require('electron-updater'); // Importing autoUpdater from electron-updater module
+const log = require('electron-log'); // Importing electron-log module
 
 let flaskProcess = null; // Variable to hold the Flask process
 const FLASK_PORT = 8080; // Port for Flask server
