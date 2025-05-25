84397f4e78751a7337491f6081ec37ad5d345cef
Steven-Ou <osteve425@gmail.com>
2025-05-24 22:43:20 -0400

    dirname

--- frontend/main.js
+++ frontend/main.js
@@ -10,4 +10,4 @@ const log = require('electron-log'); // Importing electron-log module
 const FLASK_PORT = 8080; // Port for Flask server
 const FLASK_HOST = '127.0.0.1'; // Host for Flask server
 const NEXTJS_DEV_URL ='http://localhost:3000'; // URL for Next.js development server
-const flaskAppDirectory = path.join
+const flaskAppDirectory = path.join(__dirname,)
