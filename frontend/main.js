a9cf7b3ada17837d6b69c612ba85f8c63e433fda
Steven-Ou <osteve425@gmail.com>
2025-05-26 23:56:00 -0400

    log.info starting flask server - sending info

--- frontend/main.js
+++ frontend/main.js
@@ -31,4 +31,5 @@ autoUpdater.autoDownload = false; // Disable auto-download for updates
 //--- Flask Server Mangement --- 
 function startFlaskServer() {
     log.info('Starting Flask Server...');
+    
 }
