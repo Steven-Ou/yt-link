16f1987510d4c69324a1dc1f3f20ef31724489e3
Steven-Ou <osteve425@gmail.com>
2025-05-27 21:41:13 -0400

    Quit the application, with the quit function

--- frontend/main.js
+++ frontend/main.js
@@ -48,6 +48,7 @@ function startFlaskServer() {
     } catch (error){
         log.error('Spawn failed to initiate Flask process.',error); // Log error if spawning Flask process fails
         dialog.showErrorBox('Critical Error', `Could not launch the local server (Flask): ${error.message}`); //Show error dialog if Flask process fails to start
+        app.quit();
     }
 
 }
