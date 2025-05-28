2dac643fd96bda39a74c4f585c8299050f6501ea
Steven-Ou <osteve425@gmail.com>
2025-05-27 20:01:24 -0400

    await until the tcpPort is used

--- frontend/main.js
+++ frontend/main.js
@@ -37,6 +37,7 @@ function startFlaskServer() {
 
     try{
         console.log(`Waiting for flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
+        await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
     }
 
 }
