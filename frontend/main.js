147fdda32ed504f5d5759855e5d9bd30c067a49b
Steven-Ou <osteve425@gmail.com>
2025-05-27 20:16:23 -0400

    saving this for later

--- frontend/main.js
+++ frontend/main.js
@@ -36,17 +36,17 @@ function startFlaskServer() {
     log.info(`Flask working directory: ${flaskAppDirectory}`); // Log the working directory for Flask app
 
     try{
-        console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
-        await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
-        console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
-        createWindow(); // Create the main window
-        //Calling the update function
-        checkUpdates();// Check for updates after the window is created
+      
     } catch (err){
-        
+
     }
 
 }
 
-
+ /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
+        await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
+        console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
+        createWindow(); // Create the main window
+        //Calling the update function
+        checkUpdates();// Check for updates after the window is created*/
 
