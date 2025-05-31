d830c701ef71658e0b4425fb80def4f66aeec785
Steven-Ou <osteve425@gmail.com>
2025-05-31 14:06:59 -0400

    comment

--- frontend/main.js
+++ frontend/main.js
@@ -108,7 +108,7 @@ function createWindow(){
     });
     if(isDev){ //If in development mode
         log.info(`Loading Next.js from dev server: ${NEXTJS_DEV_URL} `);// Log the message indicating loading Next.js from dev server 
-        mainWindow.loadURL(NEXTJS_DEV_URL); 
+        mainWindow.loadURL(NEXTJS_DEV_URL); // Load the Next.js development server URL
     }
 }
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
