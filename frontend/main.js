f2d0fbdbc458c741f7116cc32bba4add56d32d0f
Steven-Ou <osteve425@gmail.com>
2025-05-30 20:56:45 -0400

    Create a new BrowserWindow instance

--- frontend/main.js
+++ frontend/main.js
@@ -95,7 +95,9 @@ function stopFlaskServer(){ // Function to stop the Flask server
     }
 }
 function createWindow(){
-    
+    mainWindow = new BrowserWindow({ 
+
+    })
 }
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
