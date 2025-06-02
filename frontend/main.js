ba58ee3b88cf0e15469a3bad5ad29cb122df7ddb
Steven-Ou <osteve425@gmail.com>
2025-06-01 23:59:19 -0400

    comments

--- frontend/main.js
+++ frontend/main.js
@@ -112,7 +112,7 @@ function createWindow(){
         mainWindow.webContents.openDevTools(); // Open developer tools
     }else{ //If in production mode
         const indexPath = path.join(__dirname, 'index.html'); // For `next export`
-        log.info(`Loading Next.js from exported path: ${indexPath}`);
+        log.info(`Loading Next.js from exported path: ${indexPath}`);// Log the message indicating loading Next.js from exported path
     }
 }
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
