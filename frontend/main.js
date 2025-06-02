4194c5baac181b6369c661b58635af97eb3e57d3
Steven-Ou <osteve425@gmail.com>
2025-06-02 00:13:06 -0400

    comment

--- frontend/main.js
+++ frontend/main.js
@@ -123,7 +123,9 @@ function createWindow(){
         stopFlaskServer(); //Stop the Flask server when the main window is closed
     });
 }
-app.whenReady().then(async () => {
+app.whenReady().then(async () => {// When the app is ready
+
+})
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
         console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
