9777f67163572372837f8247d92a464c9d77e18c
Steven-Ou <osteve425@gmail.com>
2025-06-01 23:59:39 -0400

    Load the exported Next.js HTML file

--- frontend/main.js
+++ frontend/main.js
@@ -113,6 +113,7 @@ function createWindow(){
     }else{ //If in production mode
         const indexPath = path.join(__dirname, 'index.html'); // For `next export`
         log.info(`Loading Next.js from exported path: ${indexPath}`);// Log the message indicating loading Next.js from exported path
+        mainWindow.loadFile(indexPath); 
     }
 }
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
