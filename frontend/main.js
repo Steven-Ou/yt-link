788b18ce23580b697c93e9f308fa3b1a0966c45a
Steven-Ou <osteve425@gmail.com>
2025-06-05 02:28:33 -0400

    iF browserwindows all windows are length ==0

--- frontend/main.js
+++ frontend/main.js
@@ -159,6 +159,12 @@ app.whenReady().then(async () => {// When the app is ready
         stopFlaskServer(); // Stop the Flask server if it was started
         app.quit(); // Quit the application
     }
+
+    app.on(()=>{
+        if(BrowserWindow.getAllWindows().length ===0){
+            
+        }
+    });
 })
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
