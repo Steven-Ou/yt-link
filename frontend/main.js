5d95ffac96593f9f4905bb890585bf6b2b7ab5ef
Steven-Ou <osteve425@gmail.com>
2025-06-07 12:31:58 -0400

    Log the error if Flask server did not restart

--- frontend/main.js
+++ frontend/main.js
@@ -171,7 +171,7 @@ app.whenReady().then(async () => {// When the app is ready
                 tcpPortUsed.waitUntilUsed(FLASK_PORT,500,10000)//Wait until Flask server is up and running  
                     .then(() =>createWindow())//Create the main window after Flask server is up
                     .catch(errActivate=>{//If there is an error waiting for Flask server
-
+                        log.error('Flask did not restart on activate:', errActivate);
                     })
             }
         }
