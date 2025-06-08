bb19dc4513d185a8799fd83c2cd3c1800e51ab9e
Steven-Ou <osteve425@gmail.com>
2025-06-08 10:19:17 -0400

    \n

--- frontend/main.js
+++ frontend/main.js
@@ -197,6 +197,8 @@ autoUpdater.on('Checking-for-update',()=>{//Event listener for autoUpdater event
         mainWindow.webContents.send('Update-status','Checking for updates...');//Send message to renderer process about checking for updates
     }
 });
+
+
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
         console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
