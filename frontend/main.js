d9c4a746acaeb1673ee44ff94aadb91537aada42
Steven-Ou <osteve425@gmail.com>
2025-06-08 00:22:01 -0400

    closing

--- frontend/main.js
+++ frontend/main.js
@@ -183,7 +183,7 @@ app.on('window-all-closed',()=>{
     if(process.platform !== 'darwin'){
         app.quit(); //Quit the application if all windows are closed and not on macOS
     }
-})
+});
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
         console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
