90bbe755f1bb48b5ba7f53744d18f0b9fea69f1e
Steven-Ou <osteve425@gmail.com>
2025-06-08 00:52:49 -0400

    \n

--- frontend/main.js
+++ frontend/main.js
@@ -190,6 +190,8 @@ app.on('will-quit',()=>{//When the application is about to quit
     log.info('Electron app will quit.');
     stopFlaskServer(); // Stop the Flask server when the application is about to quit
 });
+
+
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
         console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
