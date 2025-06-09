ee866dcc901c0667918bc88f63f8f8a2312a2036
Steven-Ou <osteve425@gmail.com>
2025-06-08 20:43:04 -0400

    Log the message indicating update is available

--- frontend/main.js
+++ frontend/main.js
@@ -202,7 +202,7 @@ autoUpdater.on('Checking-for-update',()=>{//Event listener for autoUpdater event
 });
 
 autoUpdater.on('Update-available',(info)=>{//Event listener for update available
-
+    log.info('Updater: Update available.', info);
 });
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
