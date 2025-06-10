8043126b460bd24abd4eeb4818b5acbe5ed36ebb
Steven-Ou <osteve425@gmail.com>
2025-06-10 01:39:30 -0400

    creating communication with renderer process if needed

--- frontend/main.js
+++ frontend/main.js
@@ -300,12 +300,8 @@ autoUpdater.on('update-downloaded', (info) => { // Event listener for when updat
         }
     });
 });
- /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
-        await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
-        console.log(`Flask server detected on port ${FLASK_PORT}. Creating Window...`); // Log the message indicating Flask server is detected.
-        createWindow(); // Create the main window
-        //Calling the update function
-        checkUpdates();// Check for updates after the window is created
 
+ipcMain,on('renderer-action',(event, arg)=>{
+    
+});
 
-*/
