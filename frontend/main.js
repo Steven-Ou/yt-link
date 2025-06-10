63be298e75de852576e0a06ef3c68705bce74beb
Steven-Ou <osteve425@gmail.com>
2025-06-10 01:16:36 -0400

    adding in the download percentage

--- frontend/main.js
+++ frontend/main.js
@@ -263,6 +263,7 @@ autoUpdater.on('download-progress',(progressObj)=>{
     const speedKBs = Math.round(progressObj.bytesPerSecond/1024); // Calculate the download speed in KB/s
 
     let log_message = `Updater: Download speed: ${speedKBs} KB/s`; // Log message for download speed
+    log_message += `- Downloaded:(${percent}%)`; // Append downloaded percentage to log message
 })
  /* console.log(`Waiting for Flask server on port ${FLASK_PORT}...`); // Log the message indicating waiting for Flask server
         await tcpPortUsed.waitUntilUsed(FLASK_PORT, 5000, 1000); // Wait until the Flask server is up and running
