5d97e7b432b01f67714b7cc7c327d51074c6ff90
Steven-Ou <osteve425@gmail.com>
2025-06-16 21:22:38 -0400

    Updating

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,6 @@
-// This file should be placed at [project_root]/frontend/postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+  ],
 };
