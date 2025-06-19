4bc2d2c9d022211db0ba881194bc51eacc48f781
Steven-Ou <osteve425@gmail.com>
2025-06-18 21:13:06 -0400

    Updating the preload.js

--- frontend/preload.js
+++ frontend/preload.js
@@ -2,41 +2,34 @@
 
 const { contextBridge, ipcRenderer } = require('electron');
 
-// We are exposing a controlled API to the renderer process (your Next.js app)
-// under the name `window.electronAPI`.
+// Expose a controlled API to the renderer process (Next.js app) under `window.electronAPI`.
 contextBridge.exposeInMainWorld('electronAPI', {
-    // Each function here corresponds to an ipcMain.handle call in main.js
-    // It uses ipcRenderer.invoke which is designed to work with ipcMain.handle
-    
+    // Functions to start the different types of jobs.
     startSingleMp3Job: (args) => ipcRenderer.invoke('start-single-mp3-job', args),
-
     startPlaylistZipJob: (args) => ipcRenderer.invoke('start-playlist-zip-job', args),
-    
     startCombinePlaylistMp3Job: (args) => ipcRenderer.invoke('start-combine-playlist-mp3-job', args),
 
-    getJobStatus: (args) => ipcRenderer.invoke('get-job-status', args),
+    // Function to check the status of a job.
+    getJobStatus: (jobId) => ipcRenderer.invoke('get-job-status', jobId),
+    
+    // **NEW:** Function to trigger the save file dialog in the main process.
+    saveFile: (jobInfo) => ipcRenderer.invoke('save-file', jobInfo),
 
-    // This exposes a listener for update status messages from the main process.
+    // Listener for general update status messages from the main process.
     onUpdateStatus: (callback) => {
         const listener = (event, ...args) => callback(...args);
         ipcRenderer.on('update-status', listener);
-        
-        // Return a cleanup function to remove the listener
-        return () => {
-            ipcRenderer.removeListener('update-status', listener);
-        };
+        // Return a cleanup function to remove the listener.
+        return () => ipcRenderer.removeListener('update-status', listener);
     },
     
-    // This exposes a listener specifically for download progress.
+    // Listener specifically for download progress percentages.
     onUpdateDownloadProgress: (callback) => {
         const listener = (event, ...args) => callback(...args);
         ipcRenderer.on('update-download-progress', listener);
-        
-        return () => {
-            ipcRenderer.removeListener('update-download-progress', listener);
-        };
+        return () => ipcRenderer.removeListener('update-download-progress', listener);
     },
     
-    // This invokes the main process to trigger the update and restart.
+    // Function to trigger the application restart and update installation.
     restartAndInstall: () => ipcRenderer.send('restart-and-install'),
 });
