f09ba4c0c4866be4f3b7cdfab0d84e7459c0eddf
Steven-Ou <osteve425@gmail.com>
2025-06-20 12:27:53 -0400

    Updating

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,23 +1,20 @@
+// frontend/preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose a controlled set of functions to the renderer process (your React app).
-// This is the secure way to handle IPC in Electron.
 contextBridge.exposeInMainWorld('electron', {
-  // Method to open the "select directory" dialog
-  selectDirectory: () => ipcRenderer.invoke('select-directory'),
-  
-  // Method to start a single MP3 download job
-  startSingleMp3Job: (data) => ipcRenderer.invoke('start-single-mp3-job', data),
-  
-  // Method to start a playlist zip download job
-  startPlaylistZipJob: (data) => ipcRenderer.invoke('start-playlist-zip-job', data),
-  
-  // Method to start a combined playlist download job
-  startCombinePlaylistMp3Job: (data) => ipcRenderer.invoke('start-combine-playlist-mp3-job', data),
+    // NEWLY ADDED: Exposes the function to get the downloads path.
+    getDownloadsPath: () => ipcRenderer.invoke('get-downloads-path'),
+    
+    // Existing functions
+    startSingleMp3Job: (data) => ipcRenderer.invoke('start-single-mp3-job', data),
+    startPlaylistZipJob: (data) => ipcRenderer.invoke('start-playlist-zip-job', data),
+    startCombinePlaylistMp3Job: (data) => ipcRenderer.invoke('start-combine-playlist-mp3-job', data),
+    getJobStatus: (jobId) => ipcRenderer.invoke('get-job-status', jobId),
+    selectDirectory: () => ipcRenderer.invoke('select-directory'),
+    openFolder: (path) => ipcRenderer.invoke('open-folder', path),
 
-  // Method to get the status of any job
-  getJobStatus: (jobId) => ipcRenderer.invoke('get-job-status', jobId),
-  
-  // ADDED: Method to open a folder path
-  openFolder: (folderPath) => ipcRenderer.invoke('open-folder', folderPath),
+    // Update listeners (if you have auto-update functionality)
+    onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (_event, value) => callback(value)),
+    onUpdateError: (callback) => ipcRenderer.on('update-error', (_event, value) => callback(value)),
+    removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
 });
