340258411df102f0351ca9e3a850c271256426ca
Steven-Ou <osteve425@gmail.com>
2025-06-27 13:44:28 -0400

    Updating

--- service/app.py
+++ service/app.py
@@ -12,7 +12,6 @@ import yt_dlp
 import platform
 import subprocess
 
-# --- Globals & Setup ---
 ORIGINAL_STDOUT = sys.stdout
 ORIGINAL_STDERR = sys.stderr
 
@@ -22,7 +21,6 @@ try:
     jobs = {}
     APP_TEMP_DIR = tempfile.gettempdir()
     print(f"--- Using system temporary directory: {APP_TEMP_DIR} ---", flush=True)
-    # The PATH is now set by the main.js process, so no setup is needed here.
 
     def get_playlist_index(filename):
         try:
