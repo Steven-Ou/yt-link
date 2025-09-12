d4b32587dccb7e842dc5653a863b2c7bf18afafb
Steven-Ou <osteve425@gmail.com>
2025-09-11 23:25:13 -0400

    adding in the title and style

--- frontend/app/download/page.js
+++ frontend/app/download/page.js
@@ -44,7 +44,7 @@ function DownloadPageContent() {
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-gray-900 text-white">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex flex-col">
-        <h1></h1>
+        <h1 className="text-4xl font-bold mb-8">Download Video</h1>
         <p>
 
         </p>
