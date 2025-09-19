7e9acc6cfdfdf61e361ca81b765522703e0df49b
Steven-Ou <osteve425@gmail.com>
2025-09-19 13:53:00 -0400

    adding in to clear the cache for each run

--- service/app.py
+++ service/app.py
@@ -147,6 +147,7 @@ def start_job_endpoint():
         "http_headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         },
+        "rm_cache_dir": True,
     }
 
     if job_type == "singleVideo":
