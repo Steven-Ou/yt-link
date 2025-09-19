4b9d7c611ba45243dc1609dc5966b7570f724aa7
Steven-Ou <osteve425@gmail.com>
2025-09-18 23:15:27 -0400

    Fixing preload

--- preload.js
+++ preload.js
@@ -1,13 +1,11 @@
+// preload.js
+
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
   startJob: (payload) => ipcRenderer.invoke("start-job", payload),
   getJobStatus: (jobId) => ipcRenderer.invoke("get-job-status", jobId),
-
   getVideoFormats: (url) => ipcRenderer.invoke("get-video-formats", url),
-
-  downloadFile: (options) => ipcRenderer.invoke("download-file", options),
-  openFolder: (filePath) => ipcRenderer.invoke("open-folder", filePath),
   onBackendLog: (callback) => {
     const channel = "backend-log";
     ipcRenderer.on(channel, (event, ...args) => callback(...args));
@@ -15,8 +13,5 @@ contextBridge.exposeInMainWorld("electron", {
       ipcRenderer.removeAllListeners(channel);
     };
   },
-  onBackendReady: (callback) => {
-    ipcRenderer.on("backend-ready", callback);
-    return () => ipcRenderer.removeAllListeners("backend-ready");
-  },
+  openFolder: (filePath) => ipcRenderer.invoke("open-folder", filePath),
 });
