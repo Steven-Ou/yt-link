23e0d628cdecdbafde484c82ca4205ab2ce5959f
Steven-Ou <osteve425@gmail.com>
2025-09-19 13:53:19 -0400

    updating the http headers

--- service/app.py
+++ service/app.py
@@ -145,7 +145,8 @@ def start_job_endpoint():
         "quiet": True,
         "no_warnings": True,
         "http_headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Referer": "https://www.youtube.com/",
         },
         "rm_cache_dir": True,
     }
