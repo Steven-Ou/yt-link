f39657aa95741181e71f2b04d56a9ae20fb84319
Steven-Ou <osteve425@gmail.com>
2025-12-29 02:16:56 -0500

    adding in the new call

--- frontend/app/components/views/PlaylistZipView.js
+++ frontend/app/components/views/PlaylistZipView.js
@@ -73,7 +73,7 @@ export default function PlaylistZipView({
               )
             }
             disabled={isDownloading || !url}
-            onClick={() => handleDownload("playlistZip")}
+            onClick={onDownloadClick} 
             sx={{ height: 56 }}
           >
             {isDownloading ? "Starting..." : "Download as .zip"}
